07d36f301ff2090cdde04a7b22861f8f99c5ecfc
jynjavi <javie@DESKTOP-3P7AU7C>
2017-08-05 00:42:53 -0500

    dba

---
+++ BUENFESTPROYECTO/WebContent/jsp/DBA.sql
@@ -0,0 +1,15 @@
+
+https://db4free.net/phpMyAdmin/index.php}
+
+buen_fest_2017
+erse_2017
+P@ssw0rd_2017
+javier.vv@hotmail.com
+Nombre de la base de datos MySQL:   	
+Nombre de usuario MySQL:   	
+Contraseña de usuario MySQL:   	
+Verifique contraseña de usuario MySQL:  
+
+Base de datos: buen_fest_2017
+Nombre de usuario: erse_2017
+Correo electrónico: javier.vv@hotmail.com
