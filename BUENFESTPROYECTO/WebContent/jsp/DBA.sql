cbf14ca0726fa7d37ebcf3bf20e66db3df5a054d
jynjavi <javie@DESKTOP-3P7AU7C>
2017-08-05 01:00:24 -0500

    m

--- BUENFESTPROYECTO/WebContent/jsp/DBA.sql
+++ BUENFESTPROYECTO/WebContent/jsp/DBA.sql
@@ -13,3 +13,17 @@ Verifique contraseña de usuario MySQL:
 Base de datos: buen_fest_2017
 Nombre de usuario: erse_2017
 Correo electrónico: javier.vv@hotmail.com
+
+
+
+
+Nombre de la base de datos MySQL: buenfestest	
+Nombre de usuario MySQL:   	buenfestestnoviembre
+Contraseña de usuario MySQL:   	noviembre2017
+Verifique contraseña de usuario MySQL: 
+correo javier.vv@hotmail.com
+
+
+Base de datos: buenfestest
+Nombre de usuario: buenfestestnovie
+Correo electrónico: javier.vv@hotmail.com
